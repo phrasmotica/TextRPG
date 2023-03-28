6a90cca95faaf127b0a8448dc2ad4a656aca2d2e
Julian L <julian.lawrance@gmail.com>
2023-03-28 21:32:10 +0100

    Ensure only the topmost click zone beneath the mouse fires its click event when mouse is down

--- Assets/Scripts/Zones/ClickZone.cs
+++ Assets/Scripts/Zones/ClickZone.cs
@@ -9,7 +9,24 @@ namespace TextRPG.Zones
 
         private void OnMouseDown()
         {
-            OnClick?.Invoke();
+            if (IsClicked())
+            {
+                OnClick?.Invoke();
+            }
+        }
+
+        /// <summary>
+        /// Returns whether this click zone has the "topmost" collider that overlaps with the mouse position.
+        /// </summary>
+        private bool IsClicked()
+        {
+            var wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // camera is at z = -10, background at z = 5. Distance of 20 is sufficient
+            var distance = Vector2.Distance(Camera.main.transform.position, gameObject.transform.position) * 2;
+
+            var hit = Physics2D.Raycast(wp, Vector2.zero, distance);
+            return hit.collider != null && hit.collider.gameObject == gameObject;
         }
     }
 }
